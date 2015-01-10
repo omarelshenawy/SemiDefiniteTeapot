edab0709b0ecb0cc6ebefeb8123506cc53a5b8f0
anderpet <anderpet@kth.se>
2015-01-10 17:54:06 +0100

    Fixed previous error. New error with the gaussian.

--- Table1/Table1.m
+++ Table1/Table1.m
@@ -1,5 +1,5 @@
 %Scipt for calculating the amount of correctly classified instances,
-%Anders. 
+%Anders. Phew
 
 clear all 
 close all
@@ -13,7 +13,7 @@ for ii = 1:10
     filename = strcat('Large_Margin_',Numbers,'_',num2str(ii));
     load(filename)
 
-    facit = testLabel([1:s2 end-s2+1:end],:);
+    facit = testLabel([1:s2 end-s2+1:end],:); % True classes for the training points. 
     
     da = [data(1:810,:), trLabel([1:s end-s+1:end])];
     Training = data(1:810,:);
@@ -21,25 +21,26 @@ for ii = 1:10
     TestPoints = data(811:900,:);
     
     p=1;
-    Kf = @(x,y)PolyKer( x,y,p);
+    Kf = @(x,y) (x*y').^p+1;
 
     SVMStruct = svmtrain(Training,TrainingLabels,'kernel_function',Kf,'autoscale',false);
     groupLin = svmclassify(SVMStruct,TestPoints);
     
-    size(groupLin)
-%     p = 2; % For large margin
-%     Kf = @(x,y)PolyKer( x,y,p);
-%     ker = Kf(da,da);
-%     
-%     sigma = 1; % For large margin
-%     gamma = 1/(2*sigma^2);
-%     Kf = @(x,y)(exp(-(squareform(pdist(x,'euclidean').^2))*gamma));
-%     ker = Kf(da,da);
+    p = 2; % For large margin
+    Kf = @(x,y) (x*y').^p+1;
+    SVMStruct = svmtrain(Training,TrainingLabels,'kernel_function',Kf,'autoscale',false);
+    groupPoly = svmclassify(SVMStruct,TestPoints);
+    
+    sigma = 1; % For large margin
+    gamma = 1/(2*sigma^2);
+    Kf = @(x,y)(exp(-(x-y)*(x-y)'*gamma));
+    SVMStructG = svmtrain(Training,TrainingLabels,'kernel_function',Kf,'autoscale',false);
+    groupGauss = svmclassify(SVMStructG,TestPoints);
 
     SDEerrorrate = SDEerrorrate + sum(facit~=group)/length(facit);
     Linearerrorrate = Linearerrorrate + sum(facit~=groupLin)/length(facit);
-    Polyerrorrate = Polyerrorrate + sum(facit~=group)/length(facit);
-    Gaussianerrorrate = Gaussianerrorrate + sum(facit~=group)/length(facit);
+    Polyerrorrate = Polyerrorrate + sum(facit~=groupPoly)/length(facit);
+    Gaussianerrorrate = Gaussianerrorrate + sum(facit~=groupGauss)/length(facit);
 end
 
 SDEerrorrate = SDEerrorrate/10*100 % The average error rate in percent. 
