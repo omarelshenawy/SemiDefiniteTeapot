5c026e24f44e3ef14601b251f3c04029e042d472
Emil Wärnberg <emilwa@kth.se>
2015-01-16 16:26:05 +0100

    Ander's changes to generate figure script.

---
+++ calc_eta_and_sigma.m
@@ -0,0 +1,12 @@
+function [eta sigma]= calc_eta_and_sigma(A,k)
+    N = size(A,1);
+    assert(N>k,'Cannot find %d neighbours in a set of only %d datapoints',k,N);
+    distmatrix = squareform(pdist(A));
+    eta = spalloc(N,N,k*N); %Preallocate a sparse matrix with k*N non-zero elements
+    for i=1:N
+        [~, sortIndex] = sort(distmatrix(i,:));
+        eta(i,sortIndex(2:k+1)) = ones(1,k);
+    end
+    
+    sigma = mean(mean( reshape(distmatrix(eta==1),4,N),1));
+end
